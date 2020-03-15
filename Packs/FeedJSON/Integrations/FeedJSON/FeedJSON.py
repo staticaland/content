e0ff7fcd8bb0497b1ba5a33c768b9dd4e2ca8e24
teizenman <teizenman@paloaltonetworks.com>
2020-03-15 18:16:33 +0200

    minor changes

--- Packs/FeedJSON/Integrations/FeedJSON/FeedJSON.py
+++ Packs/FeedJSON/Integrations/FeedJSON/FeedJSON.py
@@ -8,7 +8,7 @@ def main():
     params = {k: v for k, v in demisto.params().items() if v is not None}
 
     params['feed_name_to_config'] = {
-        'JSON': {
+        params.get('url'): {
             'url': params.get('url'),
             'extractor': params.get('extractor'),
             'indicator': params.get('indicator'),
